8e568966344cf19a70da1dccef63616c08eda97b
Junle Yuan <leo905991717@gmail.com>
2023-04-11 22:03:09 -0700

    fuckkkk

---
+++ 115/scripts/background.js
@@ -0,0 +1,14 @@
+console.log("testing");
+
+async function getTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let tabs = await chrome.tabs.query(queryOptions);
+    return tabs[0].url;
+}
+
+chrome.tabs.onActivated.addListener(function () {
+    console.log("TAB UPDATED")
+    getTab().then(url => {
+        console.log(url);
+    })
+})
--- 115/scripts/content.js
+++ 115/scripts/content.js
@@ -1,7 +1,15 @@
-async function getTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let tabs = await chrome.tabs.query(queryOptions);
-    return tabs[0].url;
+// async function getTab() {
+//     let queryOptions = { active: true, currentWindow: true };
+//     let tabs = await chrome.tabs.query(queryOptions);
+//     return tabs[0].url;
+// }
+
+async function test() {
+    console.log(chrome.tabs);
 }
 
-console.log(getTab());
+(async() => {
+    // var url =  await getTab();
+    // console.log(url);
+    test();
+})()
