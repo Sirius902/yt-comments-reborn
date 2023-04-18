db5a4087588cce07e742f905bfda593003c742d0
Deeraj Gurram <dgurram@ucsc.edu>
2023-04-18 16:50:52 -0700

    Preliminary Backend Database Schema

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,8 +1,25 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS comments;
-DROP TABLE IF EXISTS video;
-DROP TABLE IF EXISTS dummy;
-CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
-CREATE TABLE users();
-CREATE TABLE comments();
-CREATE TABLE video();
+/* Create a schema in Docker, drops all tables contained inside schema with "CASCADE"*/
+DROP SCHEMA backend_reborn CASCADE;
+/*Recreate the schema*/
+CREATE SCHEMA backend_reborn;
+
+
+CREATE TABLE Users (
+    INT user_id,
+    VARCHAR (30) name,
+    PRIMARY KEY (user_id);
+);
+
+CREATE TABLE Comments (
+    INT user_id,
+    VARCHAR (1024) comment,
+    date postdate,
+    FOREIGN KEY (user_id) REFERENCES Users (user_id),
+    FOREIGN KEY (comment) REFERENCES Comments(comment);
+);
+CREATE TABLE Video (
+    VARCHAR (100) vid_url,
+    VARCHAR (1024) comment,
+    PRIMARY KEY (vid_url),
+    FOREIGN KEY (comment) REFERENCES Comments(comment);
+);
