7050af4671b04f40a3b12852e1152cbb2511b5f1
Deeraj Gurram <dgurram@ucsc.edu>
2023-04-18 17:04:15 -0700

    Fixed syntax errors

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,25 +1,25 @@
-/* Create a schema in Docker, drops all tables contained inside schema with "CASCADE"*/
-DROP SCHEMA backend_reborn CASCADE;
-/*Recreate the schema*/
+/* Create a schema in Docker, drops all tables contained inside schema with "CASCADE" */
+DROP SCHEMA IF EXISTS backend_reborn CASCADE;
+/* Recreate the schema */
 CREATE SCHEMA backend_reborn;
 
 
 CREATE TABLE Users (
     INT user_id,
-    VARCHAR (30) name,
-    PRIMARY KEY (user_id);
+    VARCHAR(30) name,
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Comments (
     INT user_id,
-    VARCHAR (1024) comment,
+    VARCHAR(1024) comment,
     date postdate,
     FOREIGN KEY (user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (comment) REFERENCES Comments(comment);
+    FOREIGN KEY (comment) REFERENCES Comments(comment)
 );
 CREATE TABLE Video (
-    VARCHAR (100) vid_url,
-    VARCHAR (1024) comment,
+    VARCHAR(100) vid_url,
+    VARCHAR(1024) comment,
     PRIMARY KEY (vid_url),
-    FOREIGN KEY (comment) REFERENCES Comments(comment);
+    FOREIGN KEY (comment) REFERENCES Comments(comment)
 );
