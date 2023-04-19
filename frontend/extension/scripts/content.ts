545d6c5ef9ff0dfeca748712252d7d40f80c6249
Ryan Kang <30352373+Kang-Sunghyun@users.noreply.github.com>
2023-04-18 17:17:56 -0700

    Revert "Implemented message passing and obtaining url"

--- frontend/extension/scripts/content.ts
+++ frontend/extension/scripts/content.ts
@@ -1,4 +1,15 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const {id, parameters} = message;
-    console.log(id + "|" + parameters);
-});
+// async function getTab() {
+//     let queryOptions = { active: true, currentWindow: true };
+//     let tabs = await chrome.tabs.query(queryOptions);
+//     return tabs[0].url;
+// }
+
+async function test() {
+    console.log(chrome.tabs);
+}
+
+(async() => {
+    // var url =  await getTab();
+    // console.log(url);
+    test();
+})();
