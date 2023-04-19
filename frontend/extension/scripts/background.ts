7bf208c47142d3ed519b48c06527f8e3f6f7272a
Junle Yuan <leo905991717@gmail.com>
2023-04-18 22:40:33 -0700

    added a scuffed way to get disabled section

--- frontend/extension/scripts/background.ts
+++ frontend/extension/scripts/background.ts
@@ -1,11 +1,13 @@
 console.log("testing");
 let queryParameters: string = "";
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == "complete") {
         if (tab.url && tab.url.includes("youtube.com/watch")) {
             queryParameters = tab.url.split("?v=")[1];
             console.log(queryParameters);
+            chrome.tabs.sendMessage(tabId, { id: tabId, parameters: queryParameters });
         }
-        chrome.tabs.sendMessage(tabId, { id: tabId, parameters: queryParameters });
+        
     }
 });
--- frontend/extension/scripts/content.ts
+++ frontend/extension/scripts/content.ts
@@ -1,4 +1,32 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener( (message, sender, sendResponse) => {
     const {id, parameters} = message;
-    console.log(id + "|" + parameters);
+    console.log(id + " | " + parameters);
+
 });
+
+addEventListener("scroll", (event) => {
+
+    console.log(document.getElementById("comments")?.getElementsByClassName("style-scope yt-formatted-string")[0]);
+
+});
+
+
+function waitForElm(selector: any) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
