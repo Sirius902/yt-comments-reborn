40e3afa1472242e616d0562197886bc8ae4cfefb
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-04-19 18:44:35 -0700

    Add eslint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,20 +4,20 @@ import yaml from 'js-yaml';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'node:fs';
 import path from 'node:path';
-import { middleware } from 'express-openapi-validator';
-import { fileURLToPath } from 'node:url';
+import {middleware} from 'express-openapi-validator';
+import {fileURLToPath} from 'node:url';
 import * as db from './db';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 const apiSpec = path.join(dirname, '../api/openapi.yaml');
 
-const apiDoc = yaml.load(fs.readFileSync(apiSpec, 'utf-8')) as any;
+const apiDoc = yaml.load(fs.readFileSync(apiSpec, 'utf-8')) as object;
 app.use('/v0/api-docs', swaggerUi.serve, swaggerUi.setup(apiDoc));
 
 app.use(
@@ -43,7 +43,7 @@ app.post('/v0/user', async (req, res) => {
     res.status(200).json(user);
 });
 
-app.use([(err, req, res, next) => {
+app.use([(err, _req, res, _next) => {
     res.status(err.status).json({
         message: err.message,
         errors: err.errors,
