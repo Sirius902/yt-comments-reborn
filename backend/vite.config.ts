10b3790621560840670659bf72756d7101336d7b
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-04-21 00:32:03 -0700

    Remove redundant build settings

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -1,12 +1,7 @@
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-    ssr: {
-        target: 'node',
-        format: 'esm',
-    },
     build: {
-        target: 'esnext',
         outDir: 'build',
         sourcemap: true,
         ssr: true,
