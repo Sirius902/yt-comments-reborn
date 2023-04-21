9c35d1026d213feb90143e8c1c29c0508cf0ffc8
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-04-21 10:02:45 -0700

    Add linter and update build config

--- frontend/extension/scripts/content.ts
+++ frontend/extension/scripts/content.ts
@@ -1,32 +1,32 @@
 chrome.runtime.onMessage.addListener( (message, sender, sendResponse) => {
     const {id, parameters} = message;
-    console.log(id + " | " + parameters);
-
+    console.log(id + ' | ' + parameters);
 });
 
-addEventListener("scroll", (event) => {
-
-    console.log(document.getElementById("comments")?.getElementsByClassName("style-scope yt-formatted-string")[0]);
-
+addEventListener('scroll', (event) => {
+    console.log(document
+        .getElementById('comments')
+        ?.getElementsByClassName('style-scope yt-formatted-string')[0]);
 });
 
-
-function waitForElm(selector: any) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
+function waitForElm(selector: string): Promise<Element> {
+    return new Promise((resolve) => {
+        const element = document.querySelector(selector);
+        if (element !== null) {
+            return resolve(element);
         }
 
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
+        const observer = new MutationObserver((mutations) => {
+            const element = document.querySelector(selector);
+            if (element !== null) {
                 observer.disconnect();
+                resolve(element);
             }
         });
 
         observer.observe(document.body, {
             childList: true,
-            subtree: true
+            subtree: true,
         });
     });
 }
