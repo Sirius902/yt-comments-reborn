02a55b53a5dad5a7ebf802313f4ef7882f849f80
Kang-Sunghyun <ryskang@ucsc.edu>
2023-04-23 16:56:07 -0700

    Figured out a concise way to extract the message div

--- frontend/extension/scripts/content.ts
+++ frontend/extension/scripts/content.ts
@@ -1,37 +1,31 @@
-const waitForElem = (selector: string): Promise<Element> => {
+const waitForElem = (parent: Element, selector: string): Promise<Element> => {
     return new Promise((resolve) => {
-        const element = document.querySelector(selector);
+        const element = parent.querySelector(selector);
         if (element !== null) {
             return resolve(element);
         }
 
-        const observer = new MutationObserver((mutations) => {
-            const element = document.querySelector(selector);
+        const observer = new MutationObserver(() => {
+            const element = parent.querySelector(selector);
             if (element !== null) {
                 observer.disconnect();
                 resolve(element);
             }
         });
 
-        observer.observe(document.body, {
+        observer.observe(parent, {
             attributes: true,
             childList: true,
-            subtree: true
+            subtree: true,
         });
     });
-}
+};
 
-chrome.runtime.onMessage.addListener( (message, sender, sendResponse) => {
-    // const {id, parameters} = message;
-    // console.log(id + ' | ' + parameters);
-    const commentsDiv: Promise<Element> = waitForElem("#comments");
-    if (commentsDiv !== null) {
-        console.log(
-            document.getElementById("comments")?.
-            getElementsByClassName("style-scope yt-formatted-string")[0]
-        );
-    }
-    
+chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
+    const {id, parameters} = message;
+    console.log(id + ' | ' + parameters);
+    waitForElem(document.body, '#message.ytd-message-renderer')
+        .then((message) => console.log(message));
 });
 
 // addEventListener('scroll', (event) => {
