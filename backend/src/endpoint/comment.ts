07ececeb5174670d17b112d9987754c63c8d6a31
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-04-23 18:59:32 -0700

    Properly type params to remove casts

--- backend/src/endpoint/comment.ts
+++ backend/src/endpoint/comment.ts
@@ -1,15 +1,18 @@
 import type {Request, Response} from 'express';
 import * as db from '../db';
 import type {Comment, CommentInfo} from '../types';
+import type {ParamsDictionary} from 'express-serve-static-core';
 
 export async function get(_req: Request, res: Response<Comment[]>) {
     const comments = await db.getComments();
     res.status(200).json(comments);
 }
 
-export async function post(req: Request, res: Response<Comment>) {
-    const body = req.body as CommentInfo;
-    const comment = await db.createComment(body);
+export async function post(
+    req: Request<ParamsDictionary, Comment, CommentInfo>,
+    res: Response<Comment>,
+) {
+    const comment = await db.createComment(req.body);
     res.status(200).json(comment);
     // TODO: Insert into video table as well.
 }
