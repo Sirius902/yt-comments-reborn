349e9d163d369375945d2a0468033dd70eaacb03
Kang-Sunghyun <ryskang@ucsc.edu>
2023-05-04 16:12:05 -0700

    Integrated adding comments and sending them to database per vid id

--- frontend/extension/scripts/App.tsx
+++ frontend/extension/scripts/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import Comment from './components/Comment';
 import './App.css';
 
@@ -16,34 +16,62 @@ export type CommentJson = {
     vid_id: string;
 };
 
+export type NewCommentJson = {
+    user_id: string;
+    comment: string;
+    reply_id: string | null;
+    vid_id: string;
+};
+
+export type ErrorResponse = {
+    code: number;
+    message: string;
+};
+
 const backendUrl = 'http://localhost:3010';
 
 const App: React.FC<Props> = ({ videoId }) => {
     const commentBox = React.useRef<HTMLTextAreaElement>(null);
     const [errorMessage, setErrorMessage] = useState('Loading...');
     const [comments, setComments] = useState<CommentJson[] | null>(null);
+    const [refetchComments, forceRefetchComments] = useReducer((x) => x + 1, 0);
+    const fetchComments = () => {
+        return fetch(`${backendUrl}/v0/comment?vid_id=${videoId}`, {
+            method: 'GET',
+            }).then((res) => {
+            if (res.ok) {
+                res.json().then((json) => setComments(json as CommentJson[]));
+            } else {
+                setErrorMessage('Failed to load comments!');
+                res.json().then((json) => console.log(json));
+            }
+        });
+    };
+
+    const postComment = (comment: NewCommentJson) => {
+        const request = new Request(`${backendUrl}/v0/comment`, {
+            method: 'POST',
+            headers: {[`Content-Type`]: 'application/json'},
+            body: JSON.stringify(comment)
+        });
+        return fetch(request).then((res) => {
+            if (res.ok) {
+                forceRefetchComments();
+                return res.json();
+            } else {
+                res.json().then((json) => console.log(json));
+            }
+        });
+    };
 
     useEffect(() => {
-            fetch(`${backendUrl}/v0/comment?vid_id=${videoId}`, {
-                method: 'GET',
-            }).then((res) => {
-                if (res.ok) {
-                    res.json().then((json) => setComments(json as CommentJson[]));
-                } else {
-                    setErrorMessage('Failed to load comments!');
-                }
-            }).catch((e) => {
-                if (e instanceof Error) {
-                    console.log(e.message);
-                } else {
-                    console.log(`oh no what is this error thing: ${e}`);
-                }
-            });
-    }, [comments]);
+            fetchComments();
+    }, [refetchComments]);
 
     const addComment = (e: React.MouseEvent<HTMLButtonElement>) => {
         if (commentBox.current !== null && commentBox.current.value !== '') {
-            console.log(`added a comment ${commentBox.current.value}`);
+            const user_id = '37c1ac27-1230-4fa6-9e0d-70fe5306c3c5';
+            postComment({user_id, comment: commentBox.current.value, reply_id: null, vid_id: videoId});
             commentBox.current.value = '';
         }
         e.preventDefault();
