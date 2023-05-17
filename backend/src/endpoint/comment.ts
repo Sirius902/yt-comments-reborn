3848cf131f9dc90063e61f7bb8f57c8f096c051a
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-05-16 18:34:46 -0700

    Add auth

--- backend/src/endpoint/comment.ts
+++ backend/src/endpoint/comment.ts
@@ -2,6 +2,7 @@ import type {Request, Response} from 'express';
 import * as db from '../db';
 import type {Comment, CommentInfo} from '../types';
 import type {ParamsDictionary} from 'express-serve-static-core';
+import type {AuthRequest} from './auth';
 
 export async function get(
     req: Request<ParamsDictionary, unknown, unknown, {vid_id: string}>,
@@ -12,9 +13,12 @@ export async function get(
 }
 
 export async function post(
-    req: Request<ParamsDictionary, Comment, CommentInfo>,
+    req: AuthRequest<ParamsDictionary, Comment, CommentInfo>,
     res: Response<Comment>
 ) {
-    const comment = await db.createComment(req.body);
+    if (req.user_id == null) {
+        throw new Error('oh no help user id is null');
+    }
+    const comment = await db.createComment(req.user_id, req.body);
     res.status(200).json(comment);
 }
